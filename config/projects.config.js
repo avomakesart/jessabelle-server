634dd61d4613607417720c662d1e16b9fcf8224f
Alvaro Castillo <alvaro_castillo@epam.com>
2020-09-18 20:20:48 -0500

    Adding project configs for heroku

--- config/projects.config.js
+++ config/projects.config.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "alvarocastle",
-  password: "BLUEcat2518",
-  host: "localhost",
+  user: "zolwzwewlzlihg",
+  password: "2af06cc868863e9c885f54ceb45fc1e0bd1dccda12c1f7f0a54f7c8aae2e6e9e",
+  host: "ec2-52-1-95-247.compute-1.amazonaws.com",
   port: 5432,
-  database: "jessapi",
+  database: "dd7n9u4dueie33",
 });
 
 module.exports = pool;
