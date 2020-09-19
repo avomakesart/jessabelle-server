373c0d59aef51aefad99054018784cfb10d02282
Alvaro Castillo <alvaro_castillo@epam.com>
2020-09-18 20:22:22 -0500

    Adding project configs for heroku

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: 'localhost',
-  USER: 'alvarocastle',
-  PASSWORD: 'BLUEcat2518',
-  DB: 'jessapi',
+  HOST: 'ec2-52-1-95-247.compute-1.amazonaws.com',
+  USER: 'zolwzwewlzlihg',
+  PASSWORD: '2af06cc868863e9c885f54ceb45fc1e0bd1dccda12c1f7f0a54f7c8aae2e6e9e',
+  DB: 'dd7n9u4dueie33',
   PORT: 5432,
   dialect: 'postgresql',
   pool: {
